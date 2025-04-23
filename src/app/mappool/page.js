af4eb3e85c543821b55afed36efafe4a554ee408
Eric Hagen <malikerkagen@gmail.com>
2025-04-23 00:47:14 -0700

    Remove old mappool layout

--- src/app/mappool/page.js
+++ src/app/mappool/page.js
@@ -1,4 +1,3 @@
-import MapCard from "@/components/mappool/MapCard";
 import db from "../api/db/connection";
 import Link from "next/link";
 import { auth } from "@/auth";
@@ -11,9 +10,6 @@ export default async function Mappool() {
    const session = await auth();
    const mapsCollection = db.collection("maps");
    const pools = await mapsCollection
-      .find({ $or: [{ active: "fresh" }, { active: "stale" }] })
-      .toArray();
-   const newpools = await mapsCollection
       .aggregate([
          { $match: { $or: [{ active: "fresh" }, { active: "stale" }] } },
          { $unwind: "$maps" },
@@ -54,7 +50,7 @@ export default async function Mappool() {
          }
       ])
       .toArray();
-   console.log(newpools);
+   console.log(pools);
    let playerRating = null;
    if (session) {
       const player = await db.collection("players").findOne({ osuid: session.user.id });
@@ -63,7 +59,7 @@ export default async function Mappool() {
 
    return (
       <div className="d-flex flex-column gap-1">
-         {newpools.map(pool => (
+         {pools.map(pool => (
             <Card key={pool._id}>
                <CardBody>
                   <CardTitle as="h1">{pool._id}</CardTitle>
@@ -154,29 +150,6 @@ export default async function Mappool() {
                </CardBody>
             </Card>
          ))}
-         <div className="mb-2">
-            <div className="fs-2">{pools.map(p => p.name).join(" | ")}</div>
-            <div className="d-flex justify-content-between">
-               <div className="d-flex gap-1">
-                  {pools.map((p, i) => (
-                     <Link key={p.name} className="ms-1" href={p.download}>
-                        Pack {i + 1}
-                     </Link>
-                  ))}
-               </div>
-               <div>
-                  <small>Highlighted maps may be selected for Score Attack</small>
-               </div>
-            </div>
-            <hr />
-         </div>
-         <div className="d-flex flex-wrap gap-2">
-            {[]
-               .concat(...pools.map(p => p.maps))
-               .sort((a, b) => averageRating(a) - averageRating(b))
-               .map(m => <MapCard key={m.id} beatmap={m} rating={playerRating} />) ||
-               "Couldn't fetch mappool"}
-         </div>
       </div>
    );
 }
