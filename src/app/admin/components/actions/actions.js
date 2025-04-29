6c2b4ec4265870567be77440b41f76ee45899459
Malikil <eboy.coltt5@gmail.com>
2025-04-28 22:18:54 -0700

    Test - make sure different libraries produce the same result when configured the same

--- src/app/admin/components/actions/actions.js
+++ src/app/admin/components/actions/actions.js
@@ -2,6 +2,7 @@
 
 import db from "@/app/api/db/connection";
 import MLR from "ml-regression-multivariate-linear";
+import { PolynomialRegressor } from "@rainij/polynomial-regression-js";
 // note https://www.npmjs.com/package/@rainij/polynomial-regression-js
 
 async function getPreviousMapScalings(mode) {
@@ -17,26 +18,29 @@ async function getPreviousMapScalings(mode) {
          //console.log([map.stars, nm.rating, hd.rating, hr.rating, dt.rating].join(", "));
       });
    }
+   const polyReg = new PolynomialRegressor(1);
+   polyReg.fit(datasets.x, datasets.y);
    // const results = {
    //    nm: new MLR(datasets.nm.x, datasets.nm.y),
    //    hd: new MLR(datasets.hd.x, datasets.hd.y),
    //    hr: new MLR(datasets.hr.x, datasets.hr.y),
    //    dt: new MLR(datasets.dt.x, datasets.dt.y)
    // };
-   return new MLR(datasets.x, datasets.y);
+   return {
+      mlr: new MLR(datasets.x, datasets.y),
+      poly: polyReg
+   };
 }
 
 export async function debug() {
-   //const oldRatings = await getPreviousMapScalings("osu");
-   const test = await db.collection("maps").findOne({ mode: "osu", active: "fresh" });
+   const predictor = await getPreviousMapScalings("osu");
+   const test = await db.collection("maps").findOne({ mode: "osu", active: "completed" });
    test.maps.forEach(map =>
       console.log(
          [
             map.stars,
-            map.ratings.nm.rating,
-            map.ratings.hd.rating,
-            map.ratings.hr.rating,
-            map.ratings.dt.rating
+            predictor.mlr.predict([map.stars, map.length, map.bpm, map.ar, map.cs])[0],
+            predictor.poly.predict([[map.stars, map.length, map.bpm, map.ar, map.cs]])[0][0]
          ].join(", ")
       )
    );
