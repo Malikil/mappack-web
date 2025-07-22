d0a23b2e4793e7003e4da98a2e2ee5e56099ed85
Eric Hagen <malikerkagen@gmail.com>
2025-07-22 02:16:29 -0700

    Fix admin difficulty reference chart

--- src/app/admin/components/difficulty-chart/actions.ts
+++ src/app/admin/components/difficulty-chart/actions.ts
@@ -1,32 +1,28 @@
 "use server";
 
-import { playersDb, mappacksDb } from "@/app/api/db/connection";
+import { playersDb, mapsDb } from "@/app/api/db/connection";
 import { auth } from "@/auth";
 
 export async function fetchScatterData() {
    const session = await auth();
-   const mode = session
-      ? (await playersDb.findOne({ osuid: session.user.id })).gamemode || "osu"
-      : "osu";
-   const pools = mappacksDb.find({ mode });
+   const mode = session ? (await playersDb.findOne({ osuid: session.user.id })).gamemode || "osu" : "osu";
+   const maps = mapsDb.find({ mode });
    const modRatios = {
       hd: 0,
       hr: 0,
       dt: 0
    };
    const chartData = { nm: [], hd: [], hr: [], dt: [] };
-   for await (const pool of pools) {
-      pool.maps.forEach(map => {
-         const { nm, hd, hr, dt } = map.ratings;
-         modRatios.hd += hd.rating / nm.rating;
-         modRatios.hr += hr.rating / nm.rating;
-         modRatios.dt += dt.rating / nm.rating;
-         Object.keys(chartData).forEach(k => {
-            chartData[k].push({
-               x: map.stars,
-               y: map.ratings[k].rating,
-               label: `${map.artist} - ${map.title} [${map.version}]`
-            });
+   for await (const map of maps) {
+      const { nm, hd, hr, dt } = map.ratings;
+      modRatios.hd += hd.rating / nm.rating;
+      modRatios.hr += hr.rating / nm.rating;
+      modRatios.dt += dt.rating / nm.rating;
+      Object.keys(chartData).forEach(k => {
+         chartData[k].push({
+            x: map.stars,
+            y: map.ratings[k].rating,
+            label: `${map.artist} - ${map.title} [${map.version}]`
          });
       });
    }
