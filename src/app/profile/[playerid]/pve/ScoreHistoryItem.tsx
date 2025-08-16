4e8a3cc9bbc4d7f97056692d07afd6befff81ee7
Eric Hagen <malikerkagen@gmail.com>
2025-08-16 11:48:51 -0700

    Collapse match history maplist

--- src/app/profile/[playerid]/pve/ScoreHistoryItem.tsx
+++ src/app/profile/[playerid]/pve/ScoreHistoryItem.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, CardImg, CardSubtitle } from "react-bootstrap";
 import Link from "next/link";
 import { buildUrl, GameMode } from "osu-web.js";
 import {
+   ArrowDown,
    ArrowDownRightCircle,
    ArrowUpRightCircle,
    DashCircle,
@@ -24,55 +25,64 @@ export default async function ScoreHistoryItem({ match, mode }: { match: PvEMatc
    return (
       <Card>
          <CardBody>
-            {match.songs && (
-               <div className="d-flex align-items-center gap-2 mb-3 px-1">
-                  <div className="fw-bold">{match.prevRating.toFixed()}</div>
-                  {match.ratingDiff > 0 ? <ArrowUpRightCircle /> : <ArrowDownRightCircle />}
-                  <div className="fw-bold">{(match.prevRating + match.ratingDiff).toFixed()}</div>
-                  <div className="d-flex align-items-center">
-                     {match.ratingDiff > 0 ? (
-                        <PlusCircle className="text-success m-1" />
-                     ) : (
-                        <DashCircle className="text-danger m-1" />
-                     )}
-                     {Math.abs(match.ratingDiff).toFixed(1)}
-                  </div>
-                  {!isNaN(match.mp) ? (
-                     <Link
-                        className="ms-auto text-decoration-none"
-                        href={buildUrl.match(match.mp)}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                     >
-                        MP{match.mp}
-                     </Link>
+            <div className="d-flex align-items-center gap-2 px-1">
+               <div className="fw-bold">{match.prevRating.toFixed()}</div>
+               {match.ratingDiff > 0 ? <ArrowUpRightCircle /> : <ArrowDownRightCircle />}
+               <div className="fw-bold">{(match.prevRating + match.ratingDiff).toFixed()}</div>
+               <div className="d-flex align-items-center">
+                  {match.ratingDiff > 0 ? (
+                     <PlusCircle className="text-success m-1" />
                   ) : (
-                     <div className="ms-auto">{match.mp}</div>
+                     <DashCircle className="text-danger m-1" />
                   )}
+                  {Math.abs(match.ratingDiff).toFixed(1)}
                </div>
-            )}
-            <div className="d-flex gap-1 flex-wrap">
-               {details.map((m, i) => (
-                  <Card key={i} className="flex-shrink-0 flex-grow-1" style={{ flexBasis: "140px" }}>
-                     <Link href={buildUrl.beatmap(m.map._id)} target="_blank" rel="noopener noreferrer">
-                        <CardImg
-                           src={`https://assets.ppy.sh/beatmaps/${m.map.setid}/covers/cover.jpg`}
-                           alt="Cover"
-                           style={{ objectFit: "cover" }}
-                        />
-                     </Link>
-                     <CardBody className="d-flex flex-column">
-                        <CardSubtitle>{m.score.toLocaleString()}</CardSubtitle>
-                        <div>{m.map.version}</div>
-                        <div className="d-flex mt-auto">
-                           <span>{m.mod.toUpperCase()}</span>
-                           {"ratings" in m.map && (
-                              <span className="ms-auto">{m.map.ratings[m.mod].rating.toFixed()}</span>
-                           )}
-                        </div>
-                     </CardBody>
-                  </Card>
-               ))}
+               <div
+                  role="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={`#collapse${match.mp}`}
+                  aria-expanded="false"
+                  aria-controls={`collapse${match.mp}`}
+               >
+                  <small className="text-decoration-underline">Details</small> <ArrowDown />
+               </div>
+               {!isNaN(match.mp) ? (
+                  <Link
+                     className="ms-auto text-decoration-none"
+                     href={buildUrl.match(match.mp)}
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                     MP{match.mp}
+                  </Link>
+               ) : (
+                  <div className="ms-auto">{match.mp}</div>
+               )}
+            </div>
+            <div className="collapse" id={`collapse${match.mp}`}>
+               <div className="d-flex gap-1 flex-wrap mt-3">
+                  {details.map((m, i) => (
+                     <Card key={i} className="flex-shrink-0 flex-grow-1" style={{ flexBasis: "140px" }}>
+                        <Link href={buildUrl.beatmap(m.map._id)} target="_blank" rel="noopener noreferrer">
+                           <CardImg
+                              src={`https://assets.ppy.sh/beatmaps/${m.map.setid}/covers/cover.jpg`}
+                              alt="Cover"
+                              style={{ objectFit: "cover" }}
+                           />
+                        </Link>
+                        <CardBody className="d-flex flex-column">
+                           <CardSubtitle>{m.score.toLocaleString()}</CardSubtitle>
+                           <div>{m.map.version}</div>
+                           <div className="d-flex mt-auto">
+                              <span>{m.mod.toUpperCase()}</span>
+                              {"ratings" in m.map && (
+                                 <span className="ms-auto">{m.map.ratings[m.mod].rating.toFixed()}</span>
+                              )}
+                           </div>
+                        </CardBody>
+                     </Card>
+                  ))}
+               </div>
             </div>
          </CardBody>
       </Card>
--- src/app/profile/[playerid]/pvp/MatchHistoryItem.tsx
+++ src/app/profile/[playerid]/pvp/MatchHistoryItem.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, CardImg, CardSubtitle } from "react-bootstrap";
 import Link from "next/link";
 import { buildUrl, GameMode } from "osu-web.js";
 import {
+   ArrowDown,
    ArrowDownRightCircle,
    ArrowUpRightCircle,
    CheckCircle,
@@ -36,84 +37,93 @@ export default async function MatchHistoryItem({ match, mode }: { match: PvPMatc
    return (
       <Card>
          <CardBody>
-            {match.songs && (
-               <div className="d-flex justify-content-between mb-3 px-1">
-                  <div className="d-flex align-items-center gap-2">
-                     <div className="fw-bold">{match.prevRating.toFixed()}</div>
-                     {match.ratingDiff > 0 ? <ArrowUpRightCircle /> : <ArrowDownRightCircle />}
-                     <div className="fw-bold">{(match.prevRating + match.ratingDiff).toFixed()}</div>
-                     <div className="d-flex align-items-center">
-                        {match.ratingDiff > 0 ? (
-                           <PlusCircle className="text-success m-1" />
-                        ) : (
-                           <DashCircle className="text-danger m-1" />
-                        )}
-                        {Math.abs(match.ratingDiff).toFixed(1)}
-                     </div>
-                  </div>
-                  <div>
-                     vs.{" "}
-                     <Link href={`/profile/${match.opponent.id}`} className="text-decoration-none">
-                        {match.opponent.name}
-                     </Link>{" "}
-                     ({match.opponent.rating?.toFixed(0)})
-                     <Link
-                        className="ms-2 text-decoration-none"
-                        href={buildUrl.match(match.mp)}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                     >
-                        MP{match.mp}
-                     </Link>
+            <div className="d-flex px-1 gap-3">
+               <div className="d-flex align-items-center gap-2">
+                  <div className="fw-bold">{match.prevRating.toFixed()}</div>
+                  {match.ratingDiff > 0 ? <ArrowUpRightCircle /> : <ArrowDownRightCircle />}
+                  <div className="fw-bold">{(match.prevRating + match.ratingDiff).toFixed()}</div>
+                  <div className="d-flex align-items-center">
+                     {match.ratingDiff > 0 ? (
+                        <PlusCircle className="text-success m-1" />
+                     ) : (
+                        <DashCircle className="text-danger m-1" />
+                     )}
+                     {Math.abs(match.ratingDiff).toFixed(1)}
                   </div>
                </div>
-            )}
-            <div className="d-flex gap-1 flex-wrap">
-               {details.map((m, i) => (
-                  <Card
-                     className={`flex-shrink-0 flex-grow-1 border-3 ${
-                        i >= (match.warmups || 0) &&
-                        `border-${m.score > m.opponentScore ? "success" : "danger"}`
-                     }`}
-                     key={i}
-                     style={{ flexBasis: "140px" }}
+               <div
+                  role="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={`#collapse${match.mp}`}
+                  aria-expanded="false"
+                  aria-controls={`collapse${match.mp}`}
+               >
+                  <small className="text-decoration-underline">Details</small> <ArrowDown />
+               </div>
+               <div className="ms-auto">
+                  vs.{" "}
+                  <Link href={`/profile/${match.opponent.id}`} className="text-decoration-none">
+                     {match.opponent.name}
+                  </Link>{" "}
+                  ({match.opponent.rating?.toFixed(0)})
+                  <Link
+                     className="ms-2 text-decoration-none"
+                     href={buildUrl.match(match.mp)}
+                     target="_blank"
+                     rel="noopener noreferrer"
                   >
-                     <Link href={buildUrl.beatmap(m.map._id)} target="_blank" rel="noopener noreferrer">
-                        <CardImg
-                           src={`https://assets.ppy.sh/beatmaps/${m.map.setid}/covers/cover.jpg`}
-                           alt="Cover"
-                           style={{ objectFit: "cover" }}
-                        />
-                     </Link>
-                     <CardBody className="d-flex flex-column">
-                        <CardSubtitle className="d-flex justify-content-between flex-wrap">
-                           <span>{m.score.toLocaleString()}</span>
-                           <span
-                              className={`mx-1 ${
-                                 i >= (match.warmups || 0) &&
-                                 `text-${m.score > m.opponentScore ? "success" : "danger"}`
-                              }`}
-                           >
-                              {i < (match.warmups || 0) ? (
-                                 <DashCircle />
-                              ) : m.score > m.opponentScore ? (
-                                 <CheckCircle />
-                              ) : (
-                                 <XCircle />
+                     MP{match.mp}
+                  </Link>
+               </div>
+            </div>
+            <div className="collapse" id={`collapse${match.mp}`}>
+               <div className="d-flex gap-1 flex-wrap mt-3">
+                  {details.map((m, i) => (
+                     <Card
+                        className={`flex-shrink-0 flex-grow-1 border-3 ${
+                           i >= (match.warmups || 0) &&
+                           `border-${m.score > m.opponentScore ? "success" : "danger"}`
+                        }`}
+                        key={i}
+                        style={{ flexBasis: "140px" }}
+                     >
+                        <Link href={buildUrl.beatmap(m.map._id)} target="_blank" rel="noopener noreferrer">
+                           <CardImg
+                              src={`https://assets.ppy.sh/beatmaps/${m.map.setid}/covers/cover.jpg`}
+                              alt="Cover"
+                              style={{ objectFit: "cover" }}
+                           />
+                        </Link>
+                        <CardBody className="d-flex flex-column">
+                           <CardSubtitle className="d-flex justify-content-between flex-wrap">
+                              <span>{m.score.toLocaleString()}</span>
+                              <span
+                                 className={`mx-1 ${
+                                    i >= (match.warmups || 0) &&
+                                    `text-${m.score > m.opponentScore ? "success" : "danger"}`
+                                 }`}
+                              >
+                                 {i < (match.warmups || 0) ? (
+                                    <DashCircle />
+                                 ) : m.score > m.opponentScore ? (
+                                    <CheckCircle />
+                                 ) : (
+                                    <XCircle />
+                                 )}
+                              </span>
+                              <span>{m.opponentScore.toLocaleString()}</span>
+                           </CardSubtitle>
+                           <div>{m.map.version}</div>
+                           <div className="d-flex mt-auto">
+                              <span>{m.mod.toUpperCase()}</span>
+                              {"ratings" in m.map && (
+                                 <span className="ms-auto">{m.map.ratings[m.mod].rating.toFixed()}</span>
                               )}
-                           </span>
-                           <span>{m.opponentScore.toLocaleString()}</span>
-                        </CardSubtitle>
-                        <div>{m.map.version}</div>
-                        <div className="d-flex mt-auto">
-                           <span>{m.mod.toUpperCase()}</span>
-                           {"ratings" in m.map && (
-                              <span className="ms-auto">{m.map.ratings[m.mod].rating.toFixed()}</span>
-                           )}
-                        </div>
-                     </CardBody>
-                  </Card>
-               ))}
+                           </div>
+                        </CardBody>
+                     </Card>
+                  ))}
+               </div>
             </div>
          </CardBody>
       </Card>
