3aa45febfe35729889daea8953cc4ceb2dc77480
Eric Hagen <malikerkagen@gmail.com>
2026-02-01 12:25:17 -0800

    Fix api route for bot requiring logged in user

--- src/app/api/db/pve/route.ts
+++ src/app/api/db/pve/route.ts
@@ -1,18 +1,27 @@
-import { submitPve } from "@/app/profile/[playerid]/pve/actions";
 import { NextRequest, NextResponse } from "next/server";
+import { mpLinksDb } from "../connection";
+import { getLobbyData } from "@/helpers/server/multiplayer";
+import { submitPveData } from "@/helpers/server/pve";
 
 export const POST = async (req: NextRequest) => {
    const auth = req.headers.get("Authorization");
-   if (auth !== process.env.MATCH_SUBMIT_AUTH)
-      return new NextResponse("Bad auth key", { status: 401 });
+   if (auth !== process.env.MATCH_SUBMIT_AUTH) return new NextResponse("Bad auth key", { status: 401 });
 
    const { mp }: { mp: number } = await req.json();
    console.log(`Add results from ${mp}`);
 
-   const formData = new FormData();
-   formData.append('mp', mp.toString());
-   const status = await submitPve(formData);
-   if (status?.http)
-      return new NextResponse(status.http.message, { status: status.http.status })
-   else return new NextResponse(null, { status: 200 });
+   if (await mpLinksDb.findOne({ _id: mp }))
+      return new NextResponse("MP link already submitted", { status: 400 });
+   try {
+      const lobby = await getLobbyData(mp);
+      const success = await submitPveData(lobby);
+      if (!success) return new NextResponse("Incomplete lobby found", { status: 400 });
+      // Add the mp link to history
+      mpLinksDb.insertOne({ _id: mp });
+
+      return new NextResponse(null, { status: 200 });
+   } catch (err) {
+      console.error(err);
+      return new NextResponse(null, { status: 500 });
+   }
 };
