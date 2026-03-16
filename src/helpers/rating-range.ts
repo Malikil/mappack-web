ed0a45b91fbd860433313b5966edabcd604a2e1c
Eric Hagen <malikerkagen@gmail.com>
2026-03-16 11:48:58 -0700

    Update targets with TWC

--- src/helpers/rating-range.ts
+++ src/helpers/rating-range.ts
@@ -7,35 +7,35 @@ const SIGMOID_WIDTH = 6;
 export const MIN_ABSOLUTE = {
    osu: 92099,
    fruits: 500000,
-   taiko: 300000,
+   taiko: 323917,
    mania: 595268,
    dtb: 5125
 };
 export const MIN_TARGETS = {
    osu: 105432,
    fruits: 500000,
-   taiko: 300000,
+   taiko: 357592,
    mania: 630832,
    dtb: 4909
 };
 export const MAX_TARGETS = {
    osu: 892519,
    fruits: 900000,
-   taiko: 900000,
+   taiko: 920871,
    mania: 957745,
    dtb: 67
 };
 export const MAX_ABSOLUTE = {
    osu: 906084,
    fruits: 900000,
-   taiko: 900000,
+   taiko: 931134,
    mania: 963416,
    dtb: 0
 };
 export const UPDATE_REFERENCE = {
    osu: "OWC'25",
    fruits: "",
-   taiko: "",
+   taiko: "TWC'26",
    mania: "MWC7K'26",
    dtb: "Oriio Cup 2026"
 };
